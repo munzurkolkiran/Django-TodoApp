e7727c2b163b8f250150ff549d0be84c1e4fc8a2
Munzur Kolkiran <munzur56@gmail.com>
2022-10-09 01:03:33 +0300

    style duzenlemeri yapildi

--- base/views.py
+++ base/views.py
@@ -1,5 +1,35 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from . models import Task
 
 
-def homePage(request):
-    return render(request, 'base/home.html')
+class TaskList(ListView):
+    model = Task
+    context_object_name = 'tasks'
+
+
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'base/task-detail.html'
+
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
